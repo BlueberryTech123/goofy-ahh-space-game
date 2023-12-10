7045cdbb6295e158e8f9028375f9aa0598530861
JesseIguess <blueberry21007@gmail.com>
2023-12-10 08:16:56 +0000

    ADdded character controller

--- main.js
+++ main.js
@@ -1,45 +1,154 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { PointerLockControls } from 'three/addons/controls/PointerLockControls.js';
 
 const scene = new THREE.Scene();
+scene.fog = new THREE.Fog(0x777777, 15, 85);
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+let debug = null;
 
 const renderer = new THREE.WebGLRenderer();
+const cock = new THREE.Clock();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 // const controls = new OrbitControls(camera, renderer.domElement);
+const controls = new PointerLockControls(camera, document.body);
 const loader = new GLTFLoader();
 
 // ========================================================================================
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add( cube );
+
+// Textures
+const grass = new THREE.TextureLoader().load("textures/grass.png");
+grass.wrapS = grass.wrapT = THREE.RepeatWrapping;
+grass.repeat.set(125, 125);
+
+// ========================================================================================
+const geometry = new THREE.PlaneGeometry(250, 250);
+const material = new THREE.MeshBasicMaterial({ color: 0xffffff, side: THREE.DoubleSide, map: grass });
+const ground = new THREE.Mesh(geometry, material);
+ground.position.set(0, 0, 0);
+ground.rotation.set(Math.PI/2, 0, Math.PI);
+scene.add(ground);
 
 camera.position.z = 5;
 // ========================================================================================
 
+let locked = false;
+controls.addEventListener("lock", () => {
+	menu.style.display = "none";
+    locked = true;
+});
+
+controls.addEventListener("unlock", () => {
+	menu.style.display = "block";
+    locked = false;
+});
 // Input shenanigans
+let horizontalAxisRaw = {left: 0, right: 0};
+let verticalAxisRaw = {up: 0, down: 0};
+
 let horizontalAxis = 0;
 let verticalAxis = 0;
+
+let sensitivity = 0.6;
+
 document.addEventListener("keydown", onKeyDown, false);
+document.addEventListener("keyup", onKeyUp, false);
+
+
+
 function onKeyDown(event) {
-    var keyCode = event.key;
-    if (keyCode == "w" || keyCode == "W" || keyCode == "ArrowUp") {
-        horizontalAxis
-    }
+    let keyCode = event.key;
+
+    if (keyCode == "w" || keyCode == "W" || keyCode == "ArrowUp") verticalAxisRaw.up = 1;
+    if (keyCode == "s" || keyCode == "S" || keyCode == "ArrowDown") verticalAxisRaw.down = 1;
+    if (keyCode == "a" || keyCode == "A" || keyCode == "ArrowLeft") horizontalAxisRaw.left = 1;
+    if (keyCode == "d" || keyCode == "D" || keyCode == "ArrowRight") horizontalAxisRaw.right = 1;
+}
+function onKeyUp(event) {
+    let keyCode = event.key;
+
+    if (keyCode == "w" || keyCode == "W" || keyCode == "ArrowUp") verticalAxisRaw.up = 0;
+    if (keyCode == "s" || keyCode == "S" || keyCode == "ArrowDown") verticalAxisRaw.down = 0;
+    if (keyCode == "a" || keyCode == "A" || keyCode == "ArrowLeft") horizontalAxisRaw.left = 0;
+    if (keyCode == "d" || keyCode == "D" || keyCode == "ArrowRight") horizontalAxisRaw.right = 0;
+}
+
+// ========================================================================================
+
+// Utility functions
+
+function twoDecPlaces(value) {
+    return Math.round(value * 100) / 100;
 }
 
 // ========================================================================================
 
 let playerPosition = new THREE.Vector3(0, 0, 0);
+let playerRotation = new THREE.Vector3(0, 0, 0);
 let cameraOffset = new THREE.Vector3(0, 2, 0);
 
 // ========================================================================================
+document.addEventListener("DOMContentLoaded", load, false);
+function load() {
+    debug = document.querySelector('#debug');
+    document.getElementById("resume").addEventListener("click", () => {
+        controls.lock();
+    });
+}
 function update() {
 	requestAnimationFrame(update);
-	renderer.render(scene, camera);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    horizontalAxis = horizontalAxisRaw.right - horizontalAxisRaw.left;
+    verticalAxis = -verticalAxisRaw.up + verticalAxisRaw.down;
+
+    let deltaTime = cock.getDelta();
+
+    // ========================================================================================
+
+    // Code here ig
+    const theta = camera.rotation.y;
+
+    const forwardMovementVector = new THREE.Vector3(0, 0, 1);
+    let cameraQuaternion = camera.quaternion.clone();
+    cameraQuaternion.x = cameraQuaternion.z = 0;
+
+    forwardMovementVector.applyQuaternion(cameraQuaternion);
+    forwardMovementVector.setY(0);
+    forwardMovementVector.multiplyScalar(verticalAxis);
+    const rightMovementVector = new THREE.Vector3(1, 0, 0);
+    rightMovementVector.applyQuaternion(cameraQuaternion);
+    rightMovementVector.setY(0);
+    rightMovementVector.multiplyScalar(horizontalAxis);
+
+    let movementVector = new THREE.Vector3().addVectors(forwardMovementVector, rightMovementVector);
+    movementVector.normalize();
+    movementVector.multiplyScalar(deltaTime * 5);
+
+    playerPosition.add(movementVector);
+    
+
+    // Set camera position
+    const finalCameraPosition = new THREE.Vector3(0, 0, 0);
+    finalCameraPosition.addVectors(playerPosition, cameraOffset);
+    camera.position.set(finalCameraPosition.x, finalCameraPosition.y, finalCameraPosition.z);
+
+    // ========================================================================================
+
+    // Display debug info
+    // console.log(debug);
+    if (debug != null) {
+        debug.innerHTML = `
+            <h3>DEBUG INFO</h3><hr>
+            Mov: H: ${horizontalAxis}, V: ${verticalAxis}<br>
+            Pos: (${twoDecPlaces(playerPosition.x)}, ${twoDecPlaces(playerPosition.y)}, ${twoDecPlaces(playerPosition.z)})<br>
+            Rot: (${twoDecPlaces(camera.rotation.x)}, ${twoDecPlaces(camera.rotation.y)}, ${twoDecPlaces(camera.rotation.z)})<br>
+            Cam: (${twoDecPlaces(finalCameraPosition.x)}, ${twoDecPlaces(finalCameraPosition.y)}, ${twoDecPlaces(finalCameraPosition.z)})<br>`;
+    }
+
+    renderer.render(scene, camera);
 }
 update();
